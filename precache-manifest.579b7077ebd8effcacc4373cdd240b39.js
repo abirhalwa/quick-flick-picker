4e71f0797fa0e30900c183b6c5dd7ff1a3a74c38
abirhalwa <abir.halwa@gmail.com>
2020-04-14 23:16:36 -0400

    Updates

---
+++ precache-manifest.579b7077ebd8effcacc4373cdd240b39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d11065cc352577067e8a1fafcadae98",
+    "url": "/quick-flick-picker/index.html"
+  },
+  {
+    "revision": "5c78d52dd3a684a6dd0c",
+    "url": "/quick-flick-picker/static/css/main.0032a13d.chunk.css"
+  },
+  {
+    "revision": "700d81cee1ea044095a2",
+    "url": "/quick-flick-picker/static/js/2.c662cf17.chunk.js"
+  },
+  {
+    "revision": "7ba5a91c6aa4ae9cc068a25fb13d169c",
+    "url": "/quick-flick-picker/static/js/2.c662cf17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c78d52dd3a684a6dd0c",
+    "url": "/quick-flick-picker/static/js/main.ff1e70d9.chunk.js"
+  },
+  {
+    "revision": "3ee6ded568b843635954",
+    "url": "/quick-flick-picker/static/js/runtime-main.b512758b.js"
+  },
+  {
+    "revision": "48b3d23669ee55337845cebcad6c1147",
+    "url": "/quick-flick-picker/static/media/qfpLogo.48b3d236.png"
+  }
+]);
